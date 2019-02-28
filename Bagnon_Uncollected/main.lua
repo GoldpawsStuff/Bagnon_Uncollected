f04c4a44627a12a9decbb7ab60ed49934f7ecf3f
Lars Norberg <lars@larsnorbergofficial.com>
2019-02-28 12:17:42 +0100

    switching to tooltip scanning to avoid false positives

--- Bagnon_Uncollected/main.lua
+++ Bagnon_Uncollected/main.lua
@@ -7,8 +7,13 @@ local MODULE =  ...
 local ADDON, Addon = MODULE:match("[^_]+"), _G[MODULE:match("[^_]+")]
 local Module = Bagnon:NewModule("ItemUncollectedAppearance", Addon)
 
+-- Tooltip used for scanning
+local ScannerTip = _G.BagnonItemInfoScannerTooltip or CreateFrame("GameTooltip", "BagnonItemInfoScannerTooltip", WorldFrame, "GameTooltipTemplate")
+local ScannerTipName = ScannerTip:GetName()
+
 -- Lua API
 local _G = _G
+local string_find = string.find
 local string_match = string.match
 local tonumber = tonumber
 
@@ -16,12 +21,26 @@ local tonumber = tonumber
 local C_TransmogCollection = _G.C_TransmogCollection
 local CreateFrame = _G.CreateFrame
 
+-- WoW Strings
+local S_TRANSMOGRIFY_STYLE_UNCOLLECTED = _G.TRANSMOGRIFY_STYLE_UNCOLLECTED
+local S_TRANSMOGRIFY_TOOLTIP_APPEARANCE_UNKNOWN = _G.TRANSMOGRIFY_TOOLTIP_APPEARANCE_UNKNOWN
+
 -- FontString & Texture Caches
 local Cache_Uncollected = {}
 
 -----------------------------------------------------------
 -- Utility Functions
 -----------------------------------------------------------
+-- Update our secret scanner tooltip with the current button
+local RefreshScanner = function(button)
+	local bag, slot = button:GetBag(), button:GetID()
+	ScannerTip.owner = button
+	ScannerTip.bag = bag
+	ScannerTip.slot = slot
+	ScannerTip:SetOwner(button, "ANCHOR_NONE")
+	ScannerTip:SetBagItem(button:GetBag(), button:GetID())
+end
+
 -- Move Pawn out of the way
 local RefreshPawn = function(button)
 	local UpgradeIcon = button.UpgradeIcon
@@ -80,31 +99,25 @@ local Update = function(self)
 		---------------------------------------------------
 		-- Uncollected Appearance
 		---------------------------------------------------
-		if (itemRarity and (itemRarity > 1)) and (not C_TransmogCollection.PlayerHasTransmog(itemID)) then 
-			local appearanceID, sourceID = C_TransmogCollection.GetItemInfo(itemID)
-			local data = sourceID and C_TransmogCollection.GetSourceInfo(sourceID)
-			if (itemRarity and (itemRarity > 1)) and (data and (data.isCollected == false)) then
-				-- figure out if we own a matching item
-				local known
-				local sources = C_TransmogCollection.GetAppearanceSources(appearanceID)
-				if sources then 
-					for i = 1, #sources do 
-						local data = C_TransmogCollection.GetSourceInfo(sources[i].sourceID) 
-						if (data and data.isCollected) then 
-							-- found a collected matching source
-							known = true 
-							break
-						end 
+		if (itemRarity and itemRarity > 1) and (not C_TransmogCollection.PlayerHasTransmog(itemID)) then 
+
+			-- Refresh the scanner only if we actually need to scan
+			RefreshScanner(self)
+
+			local unknown
+			for i = ScannerTip:NumLines(),2,-1 do 
+				local line = _G[ScannerTipName.."TextLeft"..i]
+				if line then 
+					local msg = line:GetText()
+					if msg and (string_find(msg, S_TRANSMOGRIFY_STYLE_UNCOLLECTED) or string_find(msg, S_TRANSMOGRIFY_TOOLTIP_APPEARANCE_UNKNOWN)) then
+					unknown = true
+						break
 					end
 				end
-				if (not known) then 
-					-- some items can't have their appearances learned
-					local isInfoReady, canCollect = C_TransmogCollection.PlayerCanCollectSource(sourceID)
-					if (isInfoReady and canCollect) then 
-						local Uncollected = Cache_Uncollected[self] or Cache_GetUncollected(self)
-						Uncollected:Show()
-					end
-				end
+			end 
+			if (unknown) then 
+				local Uncollected = Cache_Uncollected[self] or Cache_GetUncollected(self)
+				Uncollected:Show()
 			else 
 				if Cache_Uncollected[self] then 
 					Cache_Uncollected[self]:Hide()
