e2e8f8bc978d76e403c5e4488bee3ba05b20ec9d
Lars Norberg <github@larsnorbergofficial.com>
2022-10-13 21:02:30 +0200

    2.0.22-Release

--- Bagnon_Uncollected/main.lua
+++ Bagnon_Uncollected/main.lua
@@ -59,7 +59,7 @@ Module:AddUpdater(function(self)
 			if (not tooltip.owner or not tooltip.bag or not tooltip.slot) then
 				tooltip.owner, tooltip.bag,tooltip.slot = self, self.bag, self:GetID()
 				tooltip:SetOwner(tooltip.owner, "ANCHOR_NONE")
-				tooltip:SetBagItem(tooltip.bag, tooltip.bag)
+				tooltip:SetBagItem(tooltip.bag, tooltip.slot)
 			end
 
 			for i = tooltip:NumLines(),2,-1 do
